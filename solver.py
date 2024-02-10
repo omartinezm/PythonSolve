f166496a96e0b57bab849c8333f8f33a28db14fe
Otaivin Martínez <otaivin@gmail.com>
2024-02-10 18:24:59 -0500

    Add function fixed + Solver

--- solver.py
+++ solver.py
@@ -21,15 +21,17 @@ def reduceTerm(expr):
             if isinstance(arg1,Number) and isinstance(arg2,Number):
                 con_coef = arg1.args[0]+arg2.args[0]
                 n_args=expr.args
-                n_args.pop(n2)
+                n_args.pop(n1+n2+1)
                 n_args.pop(n1)
                 if con_coef != 0 or len(n_args)==0:
                     n_args.append(Number(con_coef))
+                if len(n_args)==1:
+                    return (n_args[0],True)
                 return (Add(*n_args),True)
             if isinstance(arg1,Variable) and isinstance(arg2,Variable):
                 con_coef = 2
                 n_args=expr.args
-                n_args.pop(n2)
+                n_args.pop(n1+n2+1)
                 n_args.pop(n1)
                 n_args.append(Product(con_coef,arg1.args[0]))
                 return (Add(*n_args),True)
@@ -43,14 +45,14 @@ def reduceTerm(expr):
                     var_coef += arg1.args[1].args[0]+1
                 if added:
                     n_args=expr.args
-                    n_args.pop(n2)
+                    n_args.pop(n1+n2+1)
                     n_args.pop(n1)
                     if var_coef==0:
                         n_args.append(Number(0))
                     elif var_coef==0:
                         n_args.append(arg2.args[0])
                     else:
-                        n_args.append(Product(Number(var_coef),arg2.args[0]))
+                        n_args.append(Product(Number(var_coef),Variable(arg2.args[0])))
                     if len(n_args)==1:
                         return (n_args[0],True)
                     else:
@@ -64,15 +66,15 @@ def reduceTerm(expr):
                     added = True
                     var_coef += arg2.args[1].args[0]+1
                 if added:
-                    n_args=expr.args
-                    n_args.pop(n2)
+                    n_args=copy.copy(expr).args
+                    n_args.pop(n1+n2+1)
                     n_args.pop(n1)
                     if var_coef==0:
                         n_args.append(Number(0))
                     elif var_coef ==1:
                         n_args.append(arg1.args[0])
                     else:
-                        n_args.append(Product(Number(var_coef),arg1.args[0]))
+                        n_args.append(Product(Number(var_coef),Variable(arg1.args[0])))
                     if len(n_args)==1:
                         return (n_args[0],True)
                     else:
@@ -84,14 +86,14 @@ def reduceTerm(expr):
                     var_coef += arg1.args[0].args[0]+arg2.args[0].args[0]
                 if added:
                     n_args=expr.args
-                    n_args.pop(n2)
+                    n_args.pop(n1+n2+1)
                     n_args.pop(n1)
                     if var_coef==0:
                         n_args.append(Number(0))
                     elif var_coef == 1:
                         n_args.append(arg1.args[1])
                     else:
-                        n_args.append(Product(Number(var_coef),arg1.args[1]))
+                        n_args.append(Product(Number(var_coef),Variable(arg1.args[1])))
                     if len(n_args)==1:
                         return (n_args[0],True)
                     else:
@@ -102,12 +104,12 @@ def reduceTerm(expr):
                     dummy_expr = Add(Product(Number(-1),arg1.args[0]),arg2)
                 elif not isinstance(arg1,Negative) and isinstance(arg2,Negative):
                     dummy_expr = Add(arg1,Product(Number(-1),arg2.args[0]))
-                else:# isinstance(arg1,Negative) and isinstance(arg2,Negative):
+                else:
                     dummy_expr = Add(Product(Number(-1),arg1.args[0]),Product(Number(-1),arg2))
                 res = reduceTerm(dummy_expr)
                 if res[1]:
                     n_args=expr.args
-                    n_args.pop(n2)
+                    n_args.pop(n1+n2+1)
                     n_args.pop(n1)
                     n_args.append(res[0])
                     if len(n_args) == 0:
@@ -133,7 +135,6 @@ def leftToRight(expr):
             else:
                 temp = Add(*n_args)
             res.append(standarize(Equal(temp,Add(right,Negative(term))) if not isinstance(term,Negative) else Equal(temp,Add(right,term.args[0]))))
-            print('---->',res[-1])
     elif isinstance(left,Difference):
         for n, arg in enumerate(left.args):
             term = arg
@@ -220,7 +221,7 @@ def measure(graph):
     right = graph.args[1]
     depthL = depth(left,[1,0])
     depthR = depth(right,[1,0])
-    res = (depthL[1]-1)**2+(1-depthL[1])**2+(1-depthR[0])**2+(1-depthL[0])**2
+    res = (1-depthL[1])**2+(1-depthL[1])**2+(1-depthR[0])**2+(1-depthL[0])**2
     return res
 
 def depth(node,p_res):
@@ -229,47 +230,63 @@ def depth(node,p_res):
     if isinstance(node,Variable) or isinstance(node,Number):
         res[1] += 1
     elif isinstance(node, Negative):
-        if isinstance(node.args[0],Number) or isinstance(node.args[0],Variable):
-            res[1] += 1
-        else:
-            res[0] += 1
-            for n in node.args:
-                res = depth(n,res)
+        res[0] += 1
+        for n in node.args:
+            res = depth(n,res)
     else:
         res[0] += 1
         for n in node.args:
             res = depth(n,res)
     return res
 
-
+###############
 oper = [reduceSum,multiplyByMinus,leftToRight,rightToLeft,productToQuotient,reduceQuotient]
+input=parse('x+3x+211=1')
+max_iter = 10
 
-input=parse('x+2=1')
+def solveDISTANCE(input):
+    """
+        This function search the path from the equation graph to a graph in the form
+            Variable('x')=Number(a)
+        
+        This search is done loking for an operation that decreaces the measure each step
+    """
+    path = [str(input)]
+    curr = input
+    curr_measure = measure(input)
+    iteration = 0
+    while curr_measure>0 and iteration<max_iter:
+        for operation in oper:
+            new = operation(curr)
+            if new[1]:
+                change = False
+                new_expr = None
+                for n in new[0]:
+                    n_measure = measure(n)
+                    if n_measure<curr_measure:
+                        change = True
+                        new_expr = n
+                        curr_measure = n_measure
+                if change:
+                    curr = new_expr
+                    path.append(str(curr))
+        iteration += 1
 
+    return path
 
 path = []
 max_depth = 10
 
-def solve(input,path,solved=False):
+def solveGRAPH(input,path):
     """
         This function search the path from the equation graph to a graph in the form
             Variable('x')=Number(a)
+        
+        This search is done by looking on a operation graph: we construct a graph with
+            nodes as equations and edges as operations then look to a node with measure
+            zero.
     """
-    curr = input
-    curr_measure = measure(input)
-    queue = [input]
-    print(curr,"->",curr_measure," (start)")
-    while queue and len(path)<max_depth:
-        curr = queue.pop()
-        break
+    # TODO
+    return None
 
-    return path
-
-inputs = ['x+x=1','-x+x=1','2x-x=1','-2x+x=-1','-2x-x=1','-2x+1=-1','x=1','x+x+1+1=0','x+x+1+1=1+1+2x+2x','x-x+1-1=1']
-for i in inputs:
-    r = parse(i)
-    s = reduceSum(parse(i))
-    if s[1]:
-        print(r,'->',s[0][0])
-
-# print(solve(input,[]))
+print(solveDISTANCE(input))
